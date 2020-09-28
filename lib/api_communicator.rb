3a50cd1502ec2dfa122aa44640403c7d3128e8e1
thesebonesalone <duikhead2@gmail.com>
2020-09-28 13:03:28 -0500

    Steamrolled project

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,24 +4,21 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
+  response_hash = parse_star_wars
+  films_array = response_hash["results"].select {|hash| hash["name"] == character_name}[0]["films"]
 
+  films_array.map do |film|
 
-  response_string = RestClient.get('http://swapi.dev/api/people')
-  response_hash = JSON.parse(response_string)
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+    info = RestClient.get(film)
+    info_hash = JSON.parse(info)
+  end
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each do |film_hash|
+    #binding.pry
+    puts film_hash["title"]
+  end
 end
 
 def show_character_movies(character)
@@ -33,3 +30,10 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+#binding.pry
+
+def parse_star_wars
+  response_string = RestClient.get('http://swapi.dev/api/people')
+  JSON.parse(response_string)
+end
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,17 @@
+require 'pry'
 def welcome
-  # puts out a welcome message here!
+  puts "Howdy Space Cowboy"
 end
 
 def get_character_from_user
   puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  response = gets.chomp
+  new_response = ''
+  response_array = response.split
+  response_array.each {|value| new_response += value.capitalize;
+    if value != response_array[-1]
+      new_response += " "
+    end
+  }
+  new_response
 end
